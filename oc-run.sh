d4a9050142f9634be4c13f21db64f6e02dd6c3a5
Anton Raizvikh <raizvih@gmail.com>
2018-08-24 10:05:03 -0700

    rename openshift-api-client to openshift-audit

--- oc-run.sh
+++ oc-run.sh
@@ -4,15 +4,11 @@ set -o nounset -o errexit
 # Runs `yarn run test` against a container on OpenShift. Used by the Test stage of the Jenkinsfile.
 # Usage: ./oc-run.sh container_name version
 
-CONTAINER_NAME="openshift-api-client"
+APP_NAME="openshift-audit"
+IMAGESTREAM=`oc get imagestream ${APP_NAME} -o='jsonpath={.status.dockerImageRepository}'`
 ENVIRONMENT=${1:-dev}
-IMAGESTREAM=`oc get imagestream ${CONTAINER_NAME} -o='jsonpath={.status.dockerImageRepository}'`
 
-# if $(oc get pod ${CONTAINER_NAME}); then
-#  oc delete pod ${CONTAINER_NAME}
-# fi
-
-oc run ${CONTAINER_NAME} \
+oc run ${APP_NAME} \
   --image=${IMAGESTREAM} \
   --rm=true \
   --attach=true \
@@ -21,14 +17,14 @@ oc run ${CONTAINER_NAME} \
   "apiVersion":"v1",
   "spec":{
     "containers":[{
-      "name": "'${CONTAINER_NAME}'",
+      "name": "'${APP_NAME}'",
       "image": "'${IMAGESTREAM}'",
       "env":[{
         "name":"API_HOST",
         "valueFrom":{
           "secretKeyRef":{
             "key": "host",
-            "name":"openshift-api-token-'${ENVIRONMENT}'"
+            "name":"'${APP_NAME}'-'${ENVIRONMENT}'"
           }
         }
       },{
@@ -36,7 +32,7 @@ oc run ${CONTAINER_NAME} \
         "valueFrom":{
           "secretKeyRef":{
             "key": "token",
-            "name":"openshift-api-token-'${ENVIRONMENT}'"
+            "name":"'${APP_NAME}'-'${ENVIRONMENT}'"
           }
         }
       }]
